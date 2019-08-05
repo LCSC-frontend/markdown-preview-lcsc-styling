2dac9f51374a30dd91b27cdd14f8ca61ab83d948
肖金 <xiaojin1992@126.com>
2019-08-05 17:23:59 +0800

    '添加对插件的预览支持，还有问题，尚未完成'

--- src/extension.ts
+++ src/extension.ts
@@ -10,17 +10,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "markdown-preview-lcsc-styling" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	return {
+		extendMarkdownIt(md: any) {
+		  	return md.use(require('markdown-it-attrs')).use(require('markdown-it-bracketed-spans'));
+		}
+	};
 }
 
 // this method is called when your extension is deactivated
